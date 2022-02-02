071e58ec506f96803311eda87fc5a40e3fdefadc
Diyar Yermek <diyar24yermek@gmail.com>
2022-02-02 01:18:54 -0800

    first bluetooth commit

--- app/src/main/java/com/example/birds_of_a_feather_team_19/MainActivity.java
+++ app/src/main/java/com/example/birds_of_a_feather_team_19/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.birds_of_a_feather_team_19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+        }
+        else {
+            int REQUEST_ENABLE_BT = 1;
+            if (!bluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
     }
 }
