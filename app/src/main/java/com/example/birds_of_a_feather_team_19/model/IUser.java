1cab1641419acdfd2eb40f376bd043b3b43d1a27
YuShuRChen <a0936582936@gmail.com>
2022-02-06 15:13:52 -0800

    Add Class UI

--- app/src/main/java/com/example/birds_of_a_feather_team_19/model/IUser.java
+++
@@ -1,9 +0,0 @@
-package com.example.birds_of_a_feather_team_19.model;
-
-import java.util.List;
-
-public abstract class IUser {
-    public abstract String getName();
-    public abstract String getPhoto();
-    public abstract List<String> getClasses();
-}
