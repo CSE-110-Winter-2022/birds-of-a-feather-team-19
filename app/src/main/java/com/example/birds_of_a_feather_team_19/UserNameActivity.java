c2cf18f31737e65faa14387dc345761006e1cacd
Akshat Lal <AkshatLal@akshats-mbp.myfiosgateway.com>
2022-02-10 17:02:12 -0500

    Added default google user name

--- app/src/main/java/com/example/birds_of_a_feather_team_19/UserNameActivity.java
+++ app/src/main/java/com/example/birds_of_a_feather_team_19/UserNameActivity.java
@@ -2,6 +2,8 @@ package com.example.birds_of_a_feather_team_19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.os.Bundle;
 
 public class UserNameActivity extends AppCompatActivity {
@@ -10,5 +12,13 @@ public class UserNameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_name);
+
+        AccountManager manager = (AccountManager) getSystemService(ACCOUNT_SERVICE);
+        Account[] listOfAccounts = manager.getAccountsByType("com.google");
+        String name = "";
+
+        if(listOfAccounts.length > 0) {
+            name = listOfAccounts[0].name;
+        }
     }
 }
