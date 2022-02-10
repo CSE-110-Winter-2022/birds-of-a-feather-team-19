84641752ab1e65702bfc5d1d8f3a7a397c3ebdc5
ZijianWeii <zwei@ucsd.edu>
2022-02-09 22:30:55 -0800

    m

--- app/src/main/java/com/example/birds_of_a_feather_team_19/AddClassActivity.java
+++ app/src/main/java/com/example/birds_of_a_feather_team_19/AddClassActivity.java
@@ -23,6 +23,7 @@ public class AddClassActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_class);
+        db = AppDatabase.singleton(this);
 
         Spinner yearSpinner = findViewById(R.id.spinnerYear);
         ArrayAdapter<CharSequence> yearAdapter =
