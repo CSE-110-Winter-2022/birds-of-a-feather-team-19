e1eda5eb8c1aa1bc356c1422d501d527ea6f0d44
dmin12 <davidmin12@gmail.com>
2022-02-13 20:11:27 -0800

    Increased frequency

--- app/src/main/java/com/example/birds_of_a_feather_team_19/MainActivity.java
+++ app/src/main/java/com/example/birds_of_a_feather_team_19/MainActivity.java
@@ -70,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
         };
         this.message = new Message("hello".getBytes(StandardCharsets.UTF_8));
 
-        this.messageListener = new MockNearbyMessageListener(realListener, 200, "Reloading");
+        this.messageListener = new MockNearbyMessageListener(realListener, 100, "Reloading");
 
         updateRecylerView();
     }
