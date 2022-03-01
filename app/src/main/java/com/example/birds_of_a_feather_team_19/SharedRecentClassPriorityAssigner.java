f2f0a4465861ce384f3904bbb9380281fa2359d1
Ethan Spraggon <ethanspraggon@Ethans-MBP.attlocal.net>
2022-02-28 23:45:48 -0800

    added age priority and recent class assigner

---
+++ app/src/main/java/com/example/birds_of_a_feather_team_19/SharedRecentClassPriorityAssigner.java
@@ -0,0 +1,28 @@
+package com.example.birds_of_a_feather_team_19;
+
+import com.example.birds_of_a_feather_team_19.model.db.Course;
+
+public class SharedRecentClassPriorityAssigner implements UserPriorityAssigner {
+
+    @Override
+    public double getPriority(Course course) {
+        switch("age") {
+            case 0:
+                return 5;
+            case 1:
+                return 4;
+            case 2:
+                return 3;
+            case 3:
+                return 2;
+            case 4:
+                return 1;
+            case 5:
+                return 0;
+        }
+        return 0;
+
+        }
+
+    }
+}
