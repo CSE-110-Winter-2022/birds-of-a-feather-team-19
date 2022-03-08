cfa778b89a4390d2d4ce2fdcfe4730489a2576ed
Akshat Lal <AkshatLal@Akshats-MacBook-Pro.local>
2022-03-08 14:56:56 -0800

    Added Session model, no DAO yet, no updates to User yet.

---
+++ app/src/main/java/com/example/birds_of_a_feather_team_19/model/db/Session.java
@@ -0,0 +1,41 @@
+package com.example.birds_of_a_feather_team_19.model.db;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "sessions")
+public class Session {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    private int id;
+
+    @ColumnInfo(name = "session_name")
+    private String sessionName;
+
+    public Session(int id, String sessionName) {
+        this.id = id;
+        this.sessionName = sessionName;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSessionName() {
+        return this.sessionName;
+    }
+
+    public void setSessionName(String sessionName) {
+        this.sessionName = sessionName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return id == ((Session) o).getId() && sessionName.equals(((Session) o).getSessionName());
+    }
+}
